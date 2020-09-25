d72e6a4683e37b911b1496078cb1ec14c2bd307a
vinaykumar7686 <vinaykumarvk.8866@gmail.com>
2020-09-25 18:35:32 +0530

    Implemented a function to return Ambient Light Value with help of captured image.

--- main.py
+++ main.py
@@ -18,8 +18,32 @@ def Capture_Gray_Image():
     else:
         return img
 
+def get_Ambient_light_Value(img):
+    '''
+    Function to return ambient lighting value.
 
+    It returns average of calculation through two methods.
 
+    Method1: Compress an image to a single pixel and extract its value.
+
+    Method2: Iterate throuch all the pixels of the image and find their average
+
+    Returned Value: (Method1+Method2)/2
+    '''
+
+    m1_val = (cv2.resize(img, (1 , 1)))[0][0]
+
+    m2_val = 0
+
+    r = len(gray)
+    c = len(rows)
+    for rows in img:
+        m2_val+=(sum(rows))/c
+
+    m2_val = m2_val/r
+
+    return (m1_val+m2_val)/2
+    
 
 
 
